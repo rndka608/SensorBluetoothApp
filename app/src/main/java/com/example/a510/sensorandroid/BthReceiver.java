9ddfc1eb00c8bde7870540e4c02e659600722060
paul <khjun325@naver.com>
2018-05-09 09:46:58 +0900

    [2018-05-02] SensorAnd #12

---
+++ app/src/main/java/com/example/a510/sensorandroid/BthReceiver.java
@@ -0,0 +1,34 @@
+package com.example.a510.sensorandroid;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class BthReceiver extends BroadcastReceiver {
+public String sName, sAddress;
+
+public BthReceiver(String sName) {
+    this.sName = sName;
+    this.sAddress = "";
+}
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+      //  throw new UnsupportedOperationException("Not yet implemented");
+    if (intent.getAction().equals(BluetoothDevice.ACTION_FOUND)) {
+        BluetoothDevice device;
+        device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+        if (device.getName().equalsIgnoreCase(sName)) {
+            sAddress = device.getAddress();
+            Toast.makeText(context, sName + "is found: " +sAddress, Toast.LENGTH_SHORT).show();
+        }
+    }
+    else if (intent.getAction().equals(BluetoothAdapter.ACTION_DISCOVERY_FINISHED)) {
+        Toast.makeText(context, "Discovery is finished" , Toast.LENGTH_SHORT).show();
+    }
+    }
+}
---
+++ app/src/main/java/com/example/a510/sensorandroid/MainActivity.java
@@ -0,0 +1,136 @@
+package com.example.a510.sensorandroid;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Set;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int BTH_ENABLE = 1010;
+    protected String sBthName = "JUNGHEE";
+    protected BluetoothAdapter bthAdapter;
+    protected BluetoothDevice bthDevice;
+    protected BluetoothManager bthManager;
+    protected BthReceiver bthReceiver;
+    protected AceBluetoothSerialService bthService;
+    protected Button btFind, btConnect, btRead, btWrite;
+    protected EditText edWrite;
+    protected TextView txRead;
+
+    protected void showMsg(String  str) {
+        Toast.makeText(this,str, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == BTH_ENABLE) {
+            if (resultCode == RESULT_OK)
+                showMsg("Bluetooth is enabled by a user.");
+            else showMsg("Bluetooth is disable by a user.");
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR2)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bthManager = (BluetoothManager)getSystemService(BLUETOOTH_SERVICE);
+        if (bthManager == null) return;
+        showMsg("BluetoothManager is found.");
+        bthAdapter = bthManager.getAdapter();
+        if (bthAdapter == null) return;
+            showMsg("BluetoothAdapter is found.");
+        if (!bthAdapter.isEnabled()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BTH_ENABLE);
+            showMsg("Bluetooth is enabled.");
+        }
+        else showMsg("Bluetooth is enabled.");
+
+        btFind = (Button) findViewById(R.id.btFind);
+        btConnect = (Button) findViewById(R.id.btConnect);
+        btRead = (Button) findViewById(R.id.btRead);
+        btWrite = (Button) findViewById(R.id.btWrite);
+        edWrite = (EditText) findViewById(R.id.edWrite);
+        txRead = (TextView) findViewById(R.id.txRead);
+
+        btFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bthAdapter.isDiscovering()) bthAdapter.cancelDiscovery();
+                bthAdapter.startDiscovery();
+                showMsg("Discovering....");
+            }
+        });
+
+        btConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (bthReceiver.sAddress.isEmpty()){
+                    showMsg("Mac address is empty.");
+                }
+                else {
+                                        bthDevice = bthAdapter.getRemoteDevice(bthReceiver.sAddress);
+                                        bthService.connect(bthDevice);
+                                       showMsg(sBthName + " is connected.");
+                                   }
+            }
+        });
+
+        btWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = edWrite.getText().toString();
+                bthService.print(str);
+            }
+        });
+
+        btRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String str = bthService.getSerialInput();
+                txRead.setText(str);
+            }
+        });
+
+        bthReceiver = new BthReceiver(sBthName);
+        IntentFilter intentFilter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(bthReceiver, intentFilter);
+        intentFilter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(bthReceiver, intentFilter);
+
+        Set<BluetoothDevice> setDevice = bthAdapter.getBondedDevices();
+                if (setDevice != null) {
+                        for (BluetoothDevice device : setDevice) {
+                                if (device.getName().equalsIgnoreCase(sBthName)) {
+                                        bthReceiver.sAddress = device.getAddress();
+                                       showMsg("MAC address of " + sBthName + "is set.");
+                                    }
+                           }         }
+
+        bthService = new AceBluetoothSerialService(this, bthAdapter);
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (bthAdapter.isDiscovering()) bthAdapter.cancelDiscovery();
+        unregisterReceiver(bthReceiver);
+        super.onDestroy();
+    }
+}
